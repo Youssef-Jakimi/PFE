5a4f29cc6fc9ed1b8d7f894dfd4b538dacafd9cc
Youssef Jakimi <jakimiyoussef@gmail.com>
2024-12-03 13:07:12 +0100

    conn push

--- database/migrations/2024_11_03_195406_create_users_table.php
+++ database/migrations/2024_11_03_195406_create_users_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('utilisateurs', function (Blueprint $table) {
+        Schema::create('users', function (Blueprint $table) {
             $table->id()->autoIncrement();
             $table->string('nom');
             $table->string('CIN')->unique();
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('utilisateurs');
+        Schema::dropIfExists('users');
     }
 };
